6f4dc808957bf4d307af6089c0b6da0e1c9f4c28
foorkan <furqanshabbir2010@gmail.com>
2020-05-07 22:43:50 -0500

    exercise

--- chapter1/FirstDialog.java
+++ chapter1/FirstDialog.java
@@ -3,6 +3,6 @@ import javax.swing.JOptionPane;
 public class FirstDialog { //class name has to be the same as file name
     public static void main(String[] args) //main method
     {
-        JOptionPane.showMessageDialog(null, "first java dialog box");
+        JOptionPane.showMessageDialog(null, "first java dialog box"); //dialog box
     }
 }
---
+++ chapter1/MovieQuoteInfo.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("The Message");
+
+    }
+    
+}
