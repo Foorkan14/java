08488dbe57af7ba9582a4ae14ba28f1b507107f0
foorkan <furqanshabbir2010@gmail.com>
2020-05-08 20:47:40 -0500

    chapter 2

---
+++ chapter2/DataDemo.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class DataDemo {
+    public static void main(String[] args)
+    {
+        int aWholenumber= 315;
+        System.out.println("The number is ");
+        System.out.println(aWholenumber);
+    }
+}
---
+++ chapter2/Datatypes.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+public class Datatypes {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+    /* Different Data types
+    int
+    string 
+    long
+    float
+    double
+    
+    
+    to declare a varible u must specify the data type first then give name of the variab;e
+    example: int myAge = 23; (since 23 is a number i use int and i dont use quotation marks around 23)
+
+    to declare a constant we must use the keyword final and all letters are capitalized
+
+    final int myAge = 23;
+
+    final double PI = 3.1245
+    */
+
+}
