618346de265944159a476cba81977682cfa2d378
foorkan <furqanshabbir2010@gmail.com>
2020-05-11 13:05:28 -0500

    code academy notes

--- chapter2/Datatypes.java
+++ chapter2/Datatypes.java
@@ -21,6 +21,22 @@ public class Datatypes {
     final int myAge = 23;
 
     final double PI = 3.1245
+
+    LEARN JAVA: MANIPULATING VARIABLES
+Review
+What’s the use of having variables if you can’t do anything with them? We’ve now seen some ways you can operate on variables and compare them. The possibilities are endless!
+
+We covered:
+
+Addition and subtraction, using + and -
+Multiplication and division, using * and /
+The modulo operator for finding remainders, %
+Greater than, >, and less than, <
+Equal to, ==, and not equal to, !=
+Greater than or equal to, >=, and less than or equal to, <=
+equals() for comparing Strings and other objects
+Using + to concatenate Strings
+Practice some of these concepts here, to make sure you have a solid foundation for learning more complicated and exciting Java concepts!
     */
 
 }
